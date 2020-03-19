744b9d62d735d837bfeb7367fc16190ae90467f3
Nishad Islam <nishad.bdg@gmail.com>
2020-03-19 20:28:25 +0600

    app added

---
+++ src/components/AppBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app-bar app color="transparent" light flat>
+
+      <v-spacer></v-spacer>
+
+      <!-- Profile Pic -->
+      <div class="d-flex align-center">
+        <v-avatar color="indigo" size="60">
+          <v-img src="../assets/profile-img.png"></v-img>
+        </v-avatar>
+      </div>
+
+      <!-- Profile Pic -->
+    </v-app-bar>
+  </div>
+</template>
