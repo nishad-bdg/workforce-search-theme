be896c5c837aed4aa7c097fc976c69988d8ae158
Nishad Islam <nishad.bdg@gmail.com>
2020-03-20 00:24:59 +0600

    search box microphone icon added

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,31 +1,48 @@
 <template>
   <div>
-   
     <v-card flat color="transparent">
       <v-card-text>
-        <v-tabs height="35" v-model="dataTabs" class="parent-tab" hide-slider>
-          <v-tab
-            v-for="tab in tabs"
-            :key="tab.id"
-            @click="currentTab = tab.id"
-            class="ct-tab-title text-capitalize mx-2"
-            :class="[
+        <v-container>
+          <v-row>
+            <v-col offset-md="2">
+              <v-card flat color="transparent">
+                <v-tabs height="35" v-model="dataTabs" class="parent-tab" hide-slider>
+                  <v-tab
+                    v-for="tab in tabs"
+                    :key="tab.id"
+                    @click="currentTab = tab.id"
+                    class="ct-tab-title text-capitalize mr-2"
+                    :class="[
                         currentTab == tab.id ? 'active-tab' : '','ct-tab',
                         tab.id == 1 ? 'round-left' : '',
                         tab.id == 2 ? 'round-right' : '',
                     ]"
-          >{{tab.title}}</v-tab>
-          <!-- tabs -->
-        </v-tabs>
-        <!-- Tab items -->
-        <v-tabs-items v-model="dataTabs">
-          <v-tab-item>
-            <v-text-field v-model="keyword" class="keyword" outlined></v-text-field>
-          </v-tab-item>
+                  >{{tab.title}}</v-tab>
+                </v-tabs>
+              </v-card>
+              <v-row no-gutters>
+                <v-col cols="10">
+                  <v-card flat>
+                    <v-tabs-items v-model="dataTabs">
+                      <v-tab-item>
+                        <v-text-field
+                          v-model="keyword"
+                          class="keyword"
+                          prepend-inner-icon="mdi-email"
+                          :append-icon="show ? 'mdi-microphone' : 'mdi-microphone-outline'"
+                          outlined
+                          @click:append = "show = !show"
+                        ></v-text-field>
+                      </v-tab-item>
 
-          <v-tab-item>Tab2</v-tab-item>
-        </v-tabs-items>
-        <!-- Tab Items -->
+                      <v-tab-item>Tab2</v-tab-item>
+                    </v-tabs-items>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-card-text>
     </v-card>
   </div>
@@ -37,6 +54,7 @@ export default {
   data() {
     return {
       keyword: "",
+      show:false,
       dataTabs: null,
       currentTab: null,
       tabs: [
@@ -65,5 +83,11 @@ export default {
 .active-tab {
   background-color: #4e6ef1;
   color: #0046fe !important;
+}
+
+.v-text-field {
+  margin-top: -1px !important;
+  background: #fdfdfd !important;
+  border-radius: 0px 10px 10px 10px !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
   <v-container fill-height fluid>
     <v-row align="center" justify="center">
-      <v-col align="center">
-        <v-img width="250" src="../assets/logo.png"></v-img>
+      <v-col align="center" cols="12">
+        <v-img width="260" src="../assets/logo.png"></v-img>
       </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
+      <v-col class="mt-n12">
         <search-bar></search-bar>
       </v-col>
     </v-row>
