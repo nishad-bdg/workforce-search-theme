fc497e917b1f626cacf9e4cd62a35c3d9bdbc02c
Nishad Islam <nishad.bdg@gmail.com>
2020-03-20 19:03:36 +0600

    search suggestion page updated

--- src/components/SuggestionResults.vue
+++ src/components/SuggestionResults.vue
@@ -4,15 +4,15 @@
       <v-list>
         <v-list-item three-line>
           <v-list-item-avatar size="80">
-            <v-img src="../assets/1.png"></v-img>
+            <v-img :src="getProfileImg(result.id)"></v-img>
           </v-list-item-avatar>
           <v-list-item-content>
             <div class="cv-url">
-              <a href="www.civ.ie/conortaylor">www.civ.ie/conortaylor</a>
+              <a :href="result.cv_url">{{result.cv_url}}</a>
             </div>
             <div class="result-title">
-              Conor Taylor . Web Developer .
-              <span>Fulltime . $20,000 . Dublin</span>
+              {{result.title}}
+              <span>{{result.designation}}</span>
             </div>
             <v-list-item-subtitle>PHP . Java . Python . Angular JS</v-list-item-subtitle>
           </v-list-item-content>
@@ -29,25 +29,31 @@ export default {
       results: [
         {
           id: 1,
-          title: "Conor Taylor . Web Developer . Fulltime . $20,000 . Dublin",
+          title: "Conor Taylor .",
+          designation: "Web Developer . Fulltime . $20,000 . Dublin",
           skills: "PHP . Java . Python . Angular JS",
-          cv_url: 'www.civ.ie/conortaylor'
+          cv_url: "www.civ.ie/conortaylor"
         },
         {
           id: 2,
-          title: "Conor Taylor . Web Developer . Fulltime . $20,000 . Dublin",
-          skills: "PHP . Java . Python . Angular JS"
+          title: "Conor Taylor .",
+          designation: "Web Developer . Fulltime . $20,000 . Dublin",
+          skills: "PHP . Java . Python . Angular JS",
+          cv_url: "https://www.linkedin.com/in/david-watson-006bb8176/"
+        },
+        {
+          id: 3,
+          title: "Conor Taylor .",
+          designation: "Web Developer . Fulltime . $20,000 . Dublin",
+          skills: "PHP . Java . Python . Angular JS",
+          cv_url: "www.civ.ie/conortaylor"
         }
       ]
     };
   },
   methods: {
     getProfileImg(id) {
-      let image = require.context(
-        "../assets/",
-        false,
-        /\.png$/
-      );
+      let image = require.context("../assets/", false, /\.png$/);
       return image("./" + id + ".png");
     }
   }
