8c948be9fb27600b405fd6c1317474afaf71ad7c
Nishad Islam <nishad.bdg@gmail.com>
2020-03-20 13:11:43 +0600

    logo width changed to percentage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
   <v-container fill-height fluid>
     <v-row align="center" justify="center">
-      <v-col align="center" cols="12">
-        <v-img width="260" src="../assets/logo.png"></v-img>
+      <v-col>
+        <v-card flat align="center" color="transparent">
+          <v-card-text>
+            <v-img width="25%" src="../assets/logo.png"></v-img>
+          </v-card-text>
+        </v-card>
       </v-col>
-      <v-col class="mt-n12">
+      <v-col class="mt-n12" cols="12">
         <search-bar></search-bar>
       </v-col>
     </v-row>
